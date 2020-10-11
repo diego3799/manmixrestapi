a203307105d0cd0f3828f80152b914d555bb36dc
diego zarate <diego3799@gmail.com>
2020-10-10 23:09:33 -0500

    finished api

--- index.js
+++ index.js
@@ -2,37 +2,49 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const GGBPool = require("./dist/GGBPool").GGBPool;
-// const GGBPool = require("node-geogebra").GGBPool;
-const fs = require("fs");
-const poolOpts = {
-  //   plotters: 5, //Number of plotters in the pool
-  ggb: "local", //local or remote: From where to load Geogebra app
-};
-
-const andGeo = "∧";
-
-const pool = new GGBPool(poolOpts);
 app.use(cors());
 app.use(bodyParser.json());
-
-app.post("/", async (req, res) => {
+const lpsolve = require("lp_solve");
+const Row = lpsolve.Row;
+const lp = new lpsolve.LinearProgram();
+app.post("/solve", async (req, res) => {
   const {
-    body: { eqLines, eqArea },
+    body: { objective, equations },
   } = req;
-  const areaPlotter = `a: x>=0 ${andGeo} y>=0 ` + eqArea.join(` ${andGeo} `);
-  console.log(areaPlotter, eqLines);
-  try {
-    await pool.ready();
-    const plotter = await pool.getGGBPlotter();
-    const ggbScript = [...eqLines, areaPlotter];
-    await plotter.evalGGBScript(ggbScript, 600, 400);
-    const buffer = await plotter.export64("png");
-    // fs.writeFileSync("dsds.png", svg64);
-    await plotter.release();
-    return res.status(200).send(buffer);
-  } catch (error) {
-    console.log(error);
+  console.log(objective);
+
+  const lp = new lpsolve.LinearProgram();
+  const x = lp.addColumn("x");
+  const y = lp.addColumn("y");
+  /**Setting the objective function */
+  lp.setObjective(new Row().Add(x, objective.x).Add(y, objective.y));
+  /**Passing all the constraints */
+  equations.forEach((item, i) => {
+    console.log(item);
+    var constraint = new Row().Add(x, item.x).Add(y, item.y);
+    lp.addConstraint(
+      constraint,
+      item.sign === ">=" ? "GE" : "LE",
+      item.z,
+      `eq${i}`
+    );
+  });
+
+  lp.dumpProgram();
+  lp.solve();
+  const result = lp.getObjectiveValue();
+  const resultX = lp.get(x);
+  const resultY = lp.get(y);
+  if (x !== null) {
+    return res.status(200).send({
+      result,
+      resultX,
+      resultY,
+    });
+  } else {
+    return res.status(400).send({
+      message: "No se encontró una solución optima",
+    });
   }
 });
 
